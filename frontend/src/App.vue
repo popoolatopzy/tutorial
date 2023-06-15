5c2f9d7bf0352a6301095fdef153fbdcc3b9e0be
popoolatopzy <57718306+popoolatopzy@users.noreply.github.com>
2023-06-15 23:13:56 +0100

    Add files via upload

---
+++ frontend/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+    <h1>Product List</h1>
+    <ul>
+      <li v-for="product in products" :key="product.id">
+        {{ product.name }} - ${{ product.price }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      products: [
+        {
+          id: 1,
+          name: "Product 1",
+          price: 10,
+        },
+        {
+          id: 2,
+          name: "Product 2",
+          price: 20,
+        },
+        {
+          id: 3,
+          name: "Product 3",
+          price: 30,
+        },
+      ],
+    };
+  },
+  mounted() {
+    this.fetchProducts();
+  },
+  methods: {
+    fetchProducts() {
+      axios
+        .get("http://localhost:3000/products")
+        .then((response) => {
+          this.products = response.data;
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
+</script>
